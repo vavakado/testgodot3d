a89cb61eda5ec317f4218a152408fc4afd8e5ec4
Vladimir Rubin <vavakado@proton.me>
2024-04-26 19:57:50 +0300

    arms rotatin' whaaat

--- resources/Button/button.gd
+++ resources/Button/button.gd
@@ -3,11 +3,17 @@ extends Node3D
 @onready var player = $"../CharacterBody3D"
 @onready var label = $Label3D
 
+signal player_entered(Node3D)
+signal player_quit()
+
 func _process(delta):
 	var distance_to_player = global_position.distance_to(player.global_position)
 	if distance_to_player < 2:
 		label.show()
 		label.look_at(player.global_position, Vector3.DOWN)
+		player_entered.emit(self)
 	else:
 		label.hide()
+		player_quit.emit()
+		
 	
